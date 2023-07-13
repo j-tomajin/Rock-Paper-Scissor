6f22617747314ed92320ed93abb9b3b2a0734743
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-13 17:49:54 +0800

    router

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import Home from '../views/Home.vue'
+import RockPaperScissor from "../views/RockPaperScissor.vue"
+import RockPaperScissorLizardSpock from "../views/RockPaperScissorLizardSpock.vue"
+import NotFoundView from '../views/NotFoundView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        }, 
+        {
+            path: '/rock-paper-scissor',
+            name: 'rock-paper-scissor',
+            component: RockPaperScissor,
+        }, 
+        {
+            path: '/rock-paper-scissor-lizard-spock',
+            name: 'bonus',
+            component: RockPaperScissorLizardSpock,
+        }, 
+        {
+          path: '/:patchMatch(.*)*',
+          name: 'notFound',
+          component: NotFoundView
+      }
+    ]
+})
+
+export default router
