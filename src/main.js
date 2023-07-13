bea840d8497ca717687e66d3e7e573dec130be9a
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-13 17:48:38 +0800

    main js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import router from './router'
+import App from './App.vue'
+
+const store = createPinia()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app')
