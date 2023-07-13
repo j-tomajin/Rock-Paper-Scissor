457ea9405b4494a299da1a15bd1899aaf3b58941
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-13 17:50:25 +0800

    NFV

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'NotFoundView',
+  }
+</script>
+
+<template>
+  <div>
+    404 - NOT FOUND
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+  div {
+    text-align: center;
+    color: white;
+  }
+</style>
