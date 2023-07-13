a5b53781b677ee00a6a278e97bce11ccf0599959
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-13 17:49:18 +0800

    App

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import { RouterView } from 'vue-router'
+  export default {
+    name: 'App',
+  }
+</script>
+
+<template>
+  <main class="main">
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @use './assets/scss/utilities/' as *;
+  .main {
+    width: min(calc(100% - 32px), rem(800));
+    margin-inline: auto;
+    padding: rem(32) 0;
+  }
+</style>
