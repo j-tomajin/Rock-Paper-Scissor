4ae2081f9e91ffb1f56ae383b65f09c77257363a
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-15 22:51:52 +0800

    reusable button

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<script>
+
+  export default {
+    name: 'Button',
+    props: {
+      msg: {
+        type: String,
+        default: 'This is a button from component'
+      },
+      bg_color: 'green'
+    },
+    methods: {
+      handleClick() {
+        this.$emit('btn-click')
+      }
+    },
+  }
+</script>
+
+<template>
+  <button 
+    @click="handleClick()"
+    :style="{backgroundColor: bg_color}"
+  >
+   {{ msg }}
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+</style>
