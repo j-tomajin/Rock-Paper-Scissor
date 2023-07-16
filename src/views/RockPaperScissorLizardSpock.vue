7066490c5fe68b017dbd515b2892d5b7f33eaaf5
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-16 21:00:06 +0800

    logic

--- src/views/RockPaperScissorLizardSpock.vue
+++ src/views/RockPaperScissorLizardSpock.vue
@@ -1,15 +1,21 @@
-<script>
+<script setup>
   import { useRouter } from 'vue-router'
+  import { useGameStore } from '../store/GameStore'
   import Header from '../components/Header.vue'
 
-  export default {
-    name: 'RockPaperScissorLizardSpock',
-    components: { Header },
-    setup() {
-      const router = useRouter()
-      return { router }
-    }
-  }
+  // icons
+  import rock from '../assets/images/icon-rock.svg'
+  import paper from '../assets/images/icon-paper.svg'
+  import scissor from '../assets/images/icon-scissors.svg'
+  import lizard from '../assets/images/icon-lizard.svg'
+  import spock from '../assets/images/icon-spock.svg'
+  import pentagon from '../assets/images/bg-pentagon.svg'
+
+  const router = useRouter()
+
+  const gameStore = useGameStore()
+
+  const { specialGame } = gameStore
 </script>
 
 <template>
@@ -18,7 +24,72 @@
     <button @click="router.back()" class="-gbtn">
       <i class="fa-solid fa-arrow-left"></i>
     </button>
-    <h1>RockPaperScissorLizardSpock</h1>
+    <!-- 
+        ----- CHORE ------
+        * IF THE USER IS CURRENTLY PLAYING IN EITHER MODE AND
+        * DECIDES TO BACK TO MAIN MENU
+        * THERE WILL BE POP UP ASKING THE USER IF THEY ARE SURE ABOUT THAT
+        * IF YES, THEN THE SCORE/PLAYER/OPPONENT PICK WILL RESET
+     -->
+    <h1>ROCK PAPER SCISSOR LIZARD SPOCK</h1>
+  </div>
+
+  <!-- from rps component -->
+  <div class="box">
+
+  <!-- Rock Paper Scissor Buttons -->
+    <div class="game">
+      <button @click="router.back()" class="-gbtn">
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
+
+      <img :src="pentagon" alt="pentagon background" class="pentagon">
+
+      <!-- SCISSOR -->
+      <router-link :to="{name: 'result'}" class="game-scissor sbtn-scissor-anim">
+        <button class="btn scissor" @click="specialGame('scissor')">
+          <div class="wrapper">
+            <img :src="scissor" alt="">
+          </div> 
+        </button>
+      </router-link>
+
+       <!-- SPOCK -->
+       <router-link :to="{name: 'result'}" class="game-spock sbtn-spock-anim">
+        <button class="btn spock" @click="specialGame('spock')">
+          <div class="wrapper">
+            <img :src="spock" alt="">
+          </div>
+        </button>
+      </router-link>
+
+      <!-- PAPER -->
+      <router-link :to="{name: 'result'}" class="game-paper sbtn-paper-anim">
+        <button class="btn paper" @click="specialGame('paper')">
+          <div class="wrapper">
+            <img :src="paper" alt="">
+          </div>
+        </button>
+      </router-link>
+      
+       <!-- LIZARD -->
+       <router-link :to="{name: 'result'}" class="game-lizard sbtn-lizard-anim">
+        <button class="btn lizard" @click="specialGame('lizard')">
+          <div class="wrapper">
+            <img :src="lizard" alt="">
+          </div>
+        </button>
+      </router-link>
+
+      <!-- ROCK -->
+      <router-link :to="{name: 'result'}" class="game-rock sbtn-rock-anim">
+        <button class="btn rock" @click="specialGame('rock')">
+          <div class="wrapper">
+            <img :src="rock" alt="">
+          </div>
+        </button>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -27,5 +98,93 @@
 
   .-gbtn {
     font-size: rem(24);
+    margin-right: auto;
+    position: absolute;
+    top: -10%;
+    left: clampf(0, 550, 780, -50);
+  }
+  
+  .box {
+    max-width: rem(800);
+    margin-inline: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .game {
+    position: relative;
+
+    width: min(100%, 500px);
+    margin-inline: auto;
+    display: grid;
+    place-content: center;
+    grid-template-columns: repeat(2, 1fr);
+
+    &-scissor {
+      grid-column: 1 / 3;
+      margin-inline: auto;
+      padding-top: 40px;
+    }
+    &-paper {
+      padding-left: 0;
+    }
+    &-lizard {
+      padding-left: 50%;
+    }
+    &-spock {
+      padding-left: 50%;
+    }
+
+    .pentagon {
+      position: absolute;
+      width: 80%;
+      top: 10%;
+      z-index: -1;
+      left: 50%;
+      translate: -50%;
+    }
+  }
+
+  .btn {
+    // position: absolute;
+
+    width: clampf(130, 375, 780, 155);
+    height: clampf(130, 375, 780, 155);
+    border-radius: 50%;
+
+    .wrapper {
+      width: clampf(90, 375, 780, 115);
+      height: clampf(90, 375, 780, 115);
+      border-radius: 50%;
+      background-color: var(--clr-neutral-w);
+      margin-inline: auto;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    img {
+      width: 50%;
+    }
+  }
+
+  .rock {
+    background-image: var(--clr-primary-rock);
+  }
+  .paper {
+    background-image: var(--clr-primary-paper);
+  }
+  .scissor {
+    background-image: var(--clr-primary-scissor);
+  }
+
+  .lizard {
+    background-image: var(--clr-primary-lizard);
+  }
+
+  .spock {
+    background-image: var(--clr-primary-spock);
   }
 </style>
