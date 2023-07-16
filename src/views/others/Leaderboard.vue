f6605495485e2ddcc81bd58a1b2e0e09dea34557
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-16 21:00:13 +0800

    added feature

---
+++ src/views/others/Leaderboard.vue
@@ -0,0 +1,34 @@
+<script>
+import { useRouter } from 'vue-router'
+export default {
+  name: 'Leaderboard',
+  setup() {
+    const router = useRouter()
+    return { router }
+  }
+}
+</script>
+
+<template>
+  <h1 class="head">LEADERBOARD</h1>
+  <div>
+    <h1>This features is not implemented yet...</h1>
+  </div>
+
+  <button @click="router.back()">Back</button>
+</template>
+
+<style lang="scss" scoped>
+  @use '../../assets/scss/utilities/' as *;
+
+  .head {
+    width: 100%;
+    border: 1px solid var(--clr-neutral-w);
+    padding-block: rem(12);
+    border-radius: rem(8);
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
---
+++ src/views/others/Options.vue
@@ -0,0 +1,33 @@
+<script>
+  import { useRouter } from 'vue-router'
+  export default {
+    name: 'Options',
+    setup() {
+      const router = useRouter()
+      return { router }
+    }
+  }
+</script>
+
+<template>
+  <h1 class="head">OPTIONS</h1>
+  <div>
+    <h1>This features is not implemented yet...</h1>
+  </div>
+
+  <button @click="router.back()">Back</button>
+</template>
+
+<style lang="scss" scoped>
+  @use '../../assets/scss/utilities/' as *;
+
+  .head {
+    width: 100%;
+    border: 1px solid var(--clr-neutral-w);
+    padding-block: rem(12);
+    border-radius: rem(8);
+  }
+  h1 {
+    text-align: center;
+  }
+</style>
