25da8a8d10a4ce7a2ebf91c19fec51af35596460
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-24 20:30:11 +0800

    removed pinia persisted state

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import router from './router'
 import App from './App.vue'
 
 const store = createPinia()
-store.use(piniaPluginPersistedState)
 
 createApp(App)
   .use(router)
