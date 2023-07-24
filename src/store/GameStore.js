533da2a23c534ee1ed9a0316db01ddb5dffc8694
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-24 20:30:22 +0800

    added high score feature

--- src/store/GameStore.js
+++ src/store/GameStore.js
@@ -5,19 +5,129 @@ export const useGameStore = defineStore('gameStore', {
   state: () => {
     // states here
     return {
-      gamemode: ref(''),
+      gamemode: "",
       score: ref(0),
       player: ref(''),
       opponent: ref(''),
-      isWinner: ref(null)
+      isWinner: ref(null),
+      // classic high score
+      classicHighScore: ref([
+        {
+          username: 'Jeck',
+          gamemode: 'classic',
+          score: 10
+        },
+        {
+          username: 'JPT',
+          gamemode: 'classic',
+          score: 9
+        },
+        {
+          username: 'JTomajin',
+          gamemode: 'classic',
+          score: 8
+        },
+        {
+          username: 'Jintoma',
+          gamemode: 'classic',
+          score: 7
+        },
+        {
+          username: 'jeckjeckjeck',
+          gamemode: 'classic',
+          score: 6
+        },
+        {
+          username: 'Is Handsome',
+          gamemode: 'classic',
+          score: 5
+        },
+        {
+          username: 'ur mum',
+          gamemode: 'classic',
+          score: 4
+        },
+        {
+          username: 'asdfghjkl',
+          gamemode: 'classic',
+          score: 3
+        },
+        {
+          username: 'u nub',
+          gamemode: 'classic',
+          score: 2
+        },
+        {
+          username: 'jkjk',
+          gamemode: 'classic',
+          score: 1
+        },
+      ]),
+      isNewHighScoreClassic: true,
+
+      // special high score
+      specialHighScore: ref([
+        {
+          username: 'Jeck',
+          gamemode: 'special',
+          score: 10
+        },
+        {
+          username: 'JPT',
+          gamemode: 'special',
+          score: 9
+        },
+        {
+          username: 'JTomajin',
+          gamemode: 'special',
+          score: 8
+        },
+        {
+          username: 'Jintoma',
+          gamemode: 'special',
+          score: 7
+        },
+        {
+          username: 'jeckjeckjeck',
+          gamemode: 'special',
+          score: 6
+        },
+        {
+          username: 'Is Handsome',
+          gamemode: 'special',
+          score: 5
+        },
+        {
+          username: 'ur mum',
+          gamemode: 'special',
+          score: 4
+        },
+        {
+          username: 'asdfghjkl',
+          gamemode: 'special',
+          score: 3
+        },
+        {
+          username: 'u nub',
+          gamemode: 'special',
+          score: 2
+        },
+        {
+          username: 'jkjk',
+          gamemode: 'special',
+          score: 1
+        },
+      ]),
     }
   },
-  persist: true,
   getters: {
     // getters here
   },
   actions: {
     // actions here
+    setGameMode(pick) { 
+      this.gamemode = pick
+    },
     classicGame(pick) {
       this.player = pick
 
@@ -174,6 +284,57 @@ export const useGameStore = defineStore('gameStore', {
       this.score = 0
       this.gamemode = ''
       this.isWinner = null
-    }
+    },
+
+    highScoreSort() {
+      if(this.gamemode === 'classic') {
+        this.classicHighScore.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+  
+        if(this.classicHighScore.length > 10) {
+          this.classicHighScore = this.classicHighScore.splice(0, 10)
+        }
+      } else if(this.gamemode === 'special') {
+        this.specialHighScore.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+  
+        if(this.specialHighScore.length > 10) {
+          this.specialHighScore = this.specialHighScore.splice(0, 10)
+        }
+      }
+
+    }, 
+
+    setHighScore(newScore) {
+      if(this.gamemode === 'classic') {
+        this.classicHighScore = [...this.classicHighScore, newScore]
+        this.highScoreSort() 
+      } else if(this.gamemode === 'special') {
+        this.specialHighScore = [...this.specialHighScore, newScore]
+        this.highScoreSort() 
+      }
+    },
+
+    checkIfHighScore() {
+      if(this.gamemode === 'classic') {
+        let minHighScore = Math.min(...this.classicHighScore.map(score => score.score))
+        console.log(minHighScore)
+        
+        if(this.score > minHighScore) {
+          this.isNewHighScoreClassic = true
+        } else {
+          this.isNewHighScoreClassic = false
+        }
+      } else if(this.gamemode === 'special') {
+        let minHighScore = Math.min(...this.specialHighScore.map(score => score.score))
+        console.log(minHighScore)
+        
+        if(this.score > minHighScore) {
+          this.isNewHighScoreClassic = true
+        } else {
+          this.isNewHighScoreClassic = false
+        }
+      }
+
+
+    },
   }
 })
